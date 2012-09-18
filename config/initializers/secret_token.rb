8dae916062a9591086aa5a13a9caf531e1a76c84
Devon Walshe <devon.walshe@gmail.com>
2012-09-18 18:51:46 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estlinlove::Application.config.secret_token = '6f59e239710f88deb5fcb3749cad3e7ad3a96bb6ec07add3a5b31a4e97f6f0bf84d3c83a3c7f7b4f99f69b2c69816e16b4658d2d1800604640fc5b4f081d9253'
